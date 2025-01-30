515a0902fd9a3e7428281ba62694c94c7cc72cba
Yash Thakre <ygtms67@gmail.com>
2025-01-30 16:48:01 +0100

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "remote-backend-tf-files"
+    bucket = "yash-remote-backend-tf-files"
     key    = "Terraform_Web_scraper_data/terraform.tfstate "
     region = "eu-central-1"
     dynamodb_table = "dynamodb-state-locking"
